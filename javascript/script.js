53265285fd7070ec3caf61421708592f73fe12ea
Yali Miranda <yalimiranda@gmail.com>
2019-12-05 15:50:14 -0800

    added questions

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What does HTML stand for?",
+        choices:["High-tier monkey lovers","Hypertype marked language","Hypertext markup language","Hyper typeface main loop"],
+        answer:"Hypertext markup language"
+    },
+    {
+        title: "for(var i = 0; i < 5; i++){console.log(i);} \n What is the last value of i printed to the console?",
+        choices:["i", "4", "5", "0"],
+        answer:"4"
+    },
+    {
+        title: "var x = 3; \n var y = \"4\" \n console.log(x+y); What will be printed to the console?",
+        choices:["7", "34", "3+4","undefined"],
+        answer:"34"
+    }
+];
